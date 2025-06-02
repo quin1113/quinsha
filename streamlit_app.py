38b0cdf6177c4d4dfaa033c5575ad07700906b81
quin1113 <quinshadeandra11@gmail.com>
2025-06-02 22:21:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("quinsha ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.image("transparent_2024-08-03T14-11-31.png", width=200)
+st.image("IMG_3118.jpeg", width=200)
